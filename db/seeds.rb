7d8978e47bbc19ef590c5d81cd8fd4415f604829
EC2 Default User <ec2-user@ip-172-31-40-212.ap-northeast-1.compute.internal>
2020-11-11 11:21:20 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 require "csv"
 
-advicemenus_csv = CSV.readlines("db/advicemenus.csv")
-advicemenus_csv.each do |row|
-  Advicemenu.create(menu: row[1], minimam_reps: row[2], max_reps: row[3], sets: row[4], group: row[5], created_at: row[6], updated_at: row[7])
+# advicemenus_csv = CSV.readlines("db/advicemenus.csv")
+# advicemenus_csv.each do |row|
+#   Advicemenu.create(menu: row[1], minimam_reps: row[2], max_reps: row[3], sets: row[4], group: row[5], created_at: row[6], updated_at: row[7])
+# end
+
+fooddates_csv = CSV.readlines("db/fooddates.csv")
+fooddates_csv.each do |row|
+  Fooddate.create(foodname: row[1], gram: row[2], kcal: row[3], protein: row[4], fat: row[5], carbo: row[6], created_at: row[7], updated_at: row[8], inputgram: row[9], memo: row[10])
 end
