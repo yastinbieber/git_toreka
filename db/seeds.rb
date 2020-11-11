fd30148250076ab8c63fc5eacbe3e65bc48c31ee
EC2 Default User <ec2-user@ip-172-31-40-212.ap-northeast-1.compute.internal>
2020-11-11 07:46:11 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,6 @@
-User.create(email: 'yastin.bieber6@gmail.com', password: '12345678')
+require "csv"
+
+advicemenus_csv = CSV.readlines("db/advicemenus.csv")
+advicemenus_csv.each do |row|
+  Advicemenu.create(menu: row[1], minimam_reps: row[2], max_reps: row[3], sets: row[4], group: row[5])
+end
