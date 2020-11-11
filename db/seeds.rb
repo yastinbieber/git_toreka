e95919f0703b914ba35e8d0d608672c90c3f74d1
EC2 Default User <ec2-user@ip-172-31-40-212.ap-northeast-1.compute.internal>
2020-11-11 07:56:51 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ require "csv"
 
 advicemenus_csv = CSV.readlines("db/advicemenus.csv")
 advicemenus_csv.each do |row|
-  Advicemenu.create(menu: row[1], minimam_reps: row[2], max_reps: row[3], sets: row[4], group: row[5])
+  Advicemenu.create(menu: row[1], minimam_reps: row[2], max_reps: row[3], sets: row[4], group: row[5], created_at: row[6], updated_at: row[7])
 end
