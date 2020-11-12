658a5979064ac43f57ad141598613d0b35b84144
EC2 Default User <ec2-user@ip-172-31-40-212.ap-northeast-1.compute.internal>
2020-11-12 00:51:07 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,17 @@ require "csv"
 #   Advicemenu.create(menu: row[1], minimam_reps: row[2], max_reps: row[3], sets: row[4], group: row[5], created_at: row[6], updated_at: row[7])
 # end
 
-# fooddates_csv = CSV.readlines("db/fooddates.csv")
-# fooddates_csv.each do |row|
-#   Fooddate.create(foodname: row[1], gram: row[2], kcal: row[3], protein: row[4], fat: row[5], carbo: row[6], inputgram: row[9], memo: row[10])
-# end
+fooddates_csv = CSV.readlines("db/fooddates.csv")
+fooddates_csv.each do |row|
+  Fooddate.create(foodname: row[1], gram: row[2], kcal: row[3], protein: row[4], fat: row[5], carbo: row[6], inputgram: row[9], memo: row[10])
+end
 
 # workoutdates_csv = CSV.readlines("db/workoutdates.csv")
 # workoutdates_csv.each do |row|
 #   Workoutdate.create(menu: row[1], part: row[2])
 # end
 
-howtovideos_csv = CSV.readlines("db/howtovideos.csv")
-howtovideos_csv.each do |row|
-  Howtovideo.create(target: row[1], menu: row[2], url: row[3] )
-end
+# howtovideos_csv = CSV.readlines("db/howtovideos.csv")
+# howtovideos_csv.each do |row|
+#   Howtovideo.create(target: row[1], menu: row[2], url: row[3] )
+# end
