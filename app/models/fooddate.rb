9423481772f2829405933bf5951a09545647c8c9
EC2 Default User <ec2-user@ip-172-31-40-212.ap-northeast-1.compute.internal>
2020-11-12 01:00:46 +0000

    modelの修正

--- app/models/fooddate.rb
+++ app/models/fooddate.rb
@@ -1,11 +1,11 @@
 class Fooddate < ApplicationRecord
 	has_many :fooddiaries
 	
-	before_save do
-		self.gram = (inputgram)/(inputgram)
-    self.kcal = (kcal)/(inputgram)
-    self.protein = (protein)/(inputgram)
-    self.fat = (fat)/(inputgram)
-    self.carbo = (carbo)/(inputgram)
-  end
+# 	before_save do
+# 		self.gram = (inputgram)/(inputgram)
+#     self.kcal = (kcal)/(inputgram)
+#     self.protein = (protein)/(inputgram)
+#     self.fat = (fat)/(inputgram)
+#     self.carbo = (carbo)/(inputgram)
+#   end
 end
