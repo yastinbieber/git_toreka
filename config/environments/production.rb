e5c4cf5768e2c0ba5c3ba9af0a334f332f1fd819
EC2 Default User <ec2-user@ip-172-31-40-212.ap-northeast-1.compute.internal>
2020-11-26 10:33:26 +0000

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,17 +96,17 @@ Rails.application.configure do
   
   config.public_file_server.enabled = true
   
-  config.action_mailer.default_url_options = { protocol: 'https', host: 'example.com' }
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => 'smtp.gmail.com',
-    :user_name => "toreka.bodymake@gmail.com", #先ほど作成した送信用gmailアドレス
-    :password => "wjrpfkveqfbtvtds", #2段階認証したアカウントで発行したアプリパスワード
-    :authentication => 'login'
-  }
+  # config.action_mailer.default_url_options = { protocol: 'https', host: 'example.com' }
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :enable_starttls_auto => true,
+  #   :address => "smtp.gmail.com",
+  #   :port => 587,
+  #   :domain => 'smtp.gmail.com',
+  #   :user_name => "toreka.bodymake@gmail.com", #先ほど作成した送信用gmailアドレス
+  #   :password => "wjrpfkveqfbtvtds", #2段階認証したアカウントで発行したアプリパスワード
+  #   :authentication => 'login'
+  # }
   
 end
